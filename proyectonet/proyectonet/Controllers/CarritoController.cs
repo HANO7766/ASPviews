35d5f929e4ca9066288d87802db3659555f6d139
renat <renat@LAPTOP-J1AFEOCC>
2022-07-15 23:34:15 -0400

    v1

---
+++ proyectonet/proyectonet/Controllers/CarritoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace proyectonet.Controllers
+{
+    public class CarritoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ proyectonet/proyectonet/Controllers/ReservaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace proyectonet.Controllers
+{
+    public class ReservaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
